a044a6689295e292041d26aeb5e9563bf0cf3b92
Galaxic Gaming <galaxicgamingyt@gmail.com>
2023-08-12 12:43:36 +0200

    fixed notify added colors checked if player is in car

--- SeatbeltNotification/client/client.lua
+++ SeatbeltNotification/client/client.lua
@@ -3,7 +3,13 @@ local isActivated = false
 -- Register the key press event
 RegisterKeyMapping('toggleAction', 'Toggle Action', 'keyboard', 'B')
 RegisterCommand('toggleAction', function()
-    isActivated = not isActivated
-    local message = isActivated and "~g~Je hebt uw gordel aangedaan" or "~r~Je hebt uw gordel uitgedaan"
-    TriggerEvent("esx:showNotification", message)
+    if IsPedInAnyVehicle(ped, false) and GetPedInVehicleSeat(GetVehiclePedIsIn(ped, false), -1) == ped then
+        isActivated = not isActivated
+        local message = isActivated and "Je hebt uw gordel aangedaan" or "Je hebt uw gordel uitgedaan"
+        if isActivated then
+            QBCore.Functions.Notify(message, "success")
+        else
+            QBCore.Functions.Notify(message, "error")
+        end
+    end
 end, false)
